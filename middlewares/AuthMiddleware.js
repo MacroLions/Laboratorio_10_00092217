c9534e74378a668fd01c9d53fff8b983c99f9eea
MacroLions <00175617@uca.edu.sv>
2018-11-09 08:46:22 -0600

    Middlewares

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,3 +1,8 @@
+const mongoose = requiere('mongoose');
+const User = requiere ("../models/users");
+const bcrypt = requiere('bcrypt');
+const AuthMiddleware = {};
+
 //middleware que verifica si una persona esta logueada
 AuthMiddleware.isAuthentication = function (req, res, next) {
     if (!req.session.user) // verificamos is existe la session
